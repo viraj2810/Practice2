39f2185f9f8ec11b0e6ef74daf180ea0162f470d
viraj2810 <98727805+viraj2810@users.noreply.github.com>
2022-10-01 17:07:49 +0530

    Update Sample1.java

--- src/Demo/Sample1.java
+++ src/Demo/Sample1.java
@@ -5,10 +5,16 @@ public class Sample1
 	public static void main(String[] args)
 	{
 		
-		System.out.println("Hi..Hello..Godd Morning");
+		System.out.println("Hi..Hello..Good Morning");
 	
 	}
 	
+	public static void m1()
+	{
+		
+		System.out.println("God Afternoon");
+	
+	}
 	
 	
 	
